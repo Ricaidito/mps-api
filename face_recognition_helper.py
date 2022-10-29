c058a0891bd1a2fb05fd909cd3dcb6a5071a8d0b
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-10-29 11:34:57 -0400

    Added todo

--- face_recognition_helper.py
+++ face_recognition_helper.py
@@ -82,6 +82,8 @@ async def recognize_by_image_file(file: UploadFile):
 
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # TODO: check the length to see if it found something
     img_econding = face_recognition.face_encodings(rgb_img)[0]
 
     results = face_recognition.compare_faces(encodings, img_econding)
